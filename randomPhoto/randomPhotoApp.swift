3903b39d1bf159d09b58d9c26165068bcb3f3ecd
michellehlcn <michellehlcn.au@gmail.com>
2024-05-14 16:19:17 +1000

    Initial Commit

---
+++ randomPhoto/randomPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomPhotoApp.swift
+//  randomPhoto
+//
+//  Created by michelle on 14/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct randomPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
