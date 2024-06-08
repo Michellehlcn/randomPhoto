4a496192b5a4792750d41dfd5dba27ec581dd5ee
michellehlcn <michellehlcn.au@gmail.com>
2024-06-08 20:36:20 +1000

    add views

---
+++ randomPhoto/Data/Data.swift
@@ -0,0 +1,8 @@
+//
+//  Data.swift
+//  randomPhoto
+//
+//  Created by michelle on 8/6/2024.
+//
+
+import Foundation
---
+++ randomPhoto/Screen/StatiscticScreen.swift
@@ -0,0 +1,20 @@
+//
+//  StatiscticScreen.swift
+//  randomPhoto
+//
+//  Created by michelle on 8/6/2024.
+//
+
+import SwiftUI
+
+struct StatiscticScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StatiscticScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        StatiscticScreen()
+    }
+}
