ebfcfdc2d2af0fb4b4cfcd444334b65e872152c3
michellehlcn <michellehlcn.au@gmail.com>
2024-06-09 21:09:09 +1000

    add more views

--- randomPhoto/Data/Data.swift
+++ randomPhoto/Data/Data.swift
@@ -7,7 +7,15 @@
 
 import Foundation
 
-//let filters: [String] = ["1 Year", "6 Months", "Last Month", "This Week"]
-//let templates:[tem] = [
-//    template(name: "Insurance "),
-//    template]
+let filters: [String] = ["1 Year", "6 Months", "Last Month", "This Week"]
+let templetes:[Templete] = [
+    Templete(name: "Insurance ", amount: "$200.80", serialNumber: "11189", colorPrimary: "OrangePrimary", colorSecondary: "OrangeSecondary"),
+    Templete(name: "Tax", amount: "$1,750", serialNumber: "11190", colorPrimary: "BluePrimary", colorSecondary: "BlueSecondary")]
+
+struct Templete {
+    let name: String
+    let amount: String
+    let serialNumber: String
+    let colorPrimary: String
+    let colorSecondary: String
+}
--- randomPhoto/Screen/StatiscticScreen.swift
+++ randomPhoto/Screen/StatiscticScreen.swift
@@ -12,9 +12,28 @@ struct StatiscticScreen: View {
         VStack{
             ScrollView(.horizontal, showsIndicators: false) {
                 LazyHStack{
-                    ForEach(filters, id:\.self)
+                    ForEach(filters, id: \.self) { filter in
+                        FilterView(title: filter)
+                        
+                    }
                 }
             }
+            .frame(height: 50)
+            .padding(.bottom, 30)
+            ZStack(alignment: .center){
+                CircularProgressView()
+                    .frame(width: 200, height: 200)
+                
+                VStack {
+                    Text("Income")
+                        .font(.headline)
+                        .foregroundColor(.gray)
+                    Text("$12,345.22")
+                        .fontWeight(.bold)
+                }
+                
+            }
+            .padding(.bottom)
         }
         .frame(maxHeight: .infinity, alignment: .top)
     }
